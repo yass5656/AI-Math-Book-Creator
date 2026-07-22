ee6c393aeff4e1f401c0dce2a3899e8766aeacdc
yass5656 <saadeldinyasser@gmail.com>
2026-07-22 07:53:35 +0300

    Add Book dataclass with title and pages attributes

--- core/models/book.py
+++ core/models/book.py
@@ -1,1 +1,12 @@
+from dataclasses import dataclass, field
+from typing import List
 
+from .page import Page
+
+
+@dataclass
+class Book:
+
+    title: str
+
+    pages: List[Page] = field(default_factory=list)
