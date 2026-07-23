392c9805ad627c4a4e5cb8757b9ff652d53ef5e7
yass5656 <saadeldinyasser@gmail.com>
2026-07-23 04:39:58 +0300

    Add stage and term attributes to Book model

--- core/models/book.py
+++ core/models/book.py
@@ -3,10 +3,9 @@ from typing import List
 
 from .page import Page
 
-
 @dataclass
 class Book:
-
     title: str
-
+    stage: int
+    term: int
     pages: List[Page] = field(default_factory=list)
