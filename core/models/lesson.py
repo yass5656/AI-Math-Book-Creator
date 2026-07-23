cdd988c8a55061b34918cb5aec544d72e9783eaf
yass5656 <saadeldinyasser@gmail.com>
2026-07-23 19:18:47 +0300

    Add Lesson class with initializer

--- core/models/lesson.py
+++ core/models/lesson.py
@@ -1,1 +1,17 @@
+class Lesson:
 
+    def __init__(
+        self,
+        title,
+        stage,
+        objectives,
+        concepts,
+        examples,
+        exercises
+    ):
+        self.title = title
+        self.stage = stage
+        self.objectives = objectives
+        self.concepts = concepts
+        self.examples = examples
+        self.exercises = exercises
