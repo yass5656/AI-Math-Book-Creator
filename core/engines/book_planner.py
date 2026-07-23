cf2d5b7cf2f089893aab3d0253edc5f8a40f0748
yass5656 <saadeldinyasser@gmail.com>
2026-07-23 04:41:12 +0300

    Refactor BookPlanner to create Book instances

--- core/engines/book_planner.py
+++ core/engines/book_planner.py
@@ -1,25 +1,11 @@
-from core.models.book_request import BookRequest
-
+from core.models.book import Book
 
 class BookPlanner:
 
-    def build(self, request: BookRequest):
+    def create(self, title, stage, term):
 
-        return {
-
-            "title": f"{request.curriculum} Stage {request.stage} Term {request.term}",
-
-            "book_type": request.book_type,
-
-            "theme": request.theme,
-
-            "units": [
-
-                {
-                    "unit": 1,
-                    "pages": []
-                }
-
-            ]
-
-        }
+        return Book(
+            title=title,
+            stage=stage,
+            term=term
+        )
